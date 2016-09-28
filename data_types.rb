8179102037276dbce659a75af56396718518c86e
Sterling Kahn <sterling@stripe.com>
2016-09-27 19:35:38 -0700

    Edited Readme and added data_types.rb for Ruby tutorial

---
+++ data_types.rb
@@ -0,0 +1,20 @@
+#Data_types
+
+#Booleans
+true_boolean = true
+false_boolean = false
+
+#Integer
+new_integer = 5
+
+#Strings
+new_string = "This is me now"
+
+#Arrays
+new_array_of_animals = ["dog", "cat", "goose"]
+
+#Hashes
+new_hash_of_dogs = {
+  "Fido" => 3,
+  "Rufus" => 5
+}
